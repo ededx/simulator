dea555bede26f4f971b1ef4afc4a7e6c9fb89cf5
ededx <117166001+ededx@users.noreply.github.com>
2024-09-27 18:10:10 +0200

    Delete S1111111sdijhfvdy765DFxwwq4.ps1

--- S1111111sdijhfvdy765DFxwwq4.ps1
+++
@@ -1,5 +0,0 @@
-Add-Type -AssemblyName PresentationCore,PresentationFramework
-$msgBody = "This is a simple message with just the default OK button"
-[System.Windows.MessageBox]::Show($msgBody)
-Write-Output "helllllllllo"
-Start-Sleep -Seconds 10
